9e5b0b09576d5c775963361e5a0423565cbac8e0
SACHIN.BAGHEL <sachin.baghel@newvisionsoftware.in>
2022-07-19 19:35:18 +0530

    PBCS-134 Code optimized

--- frontend/src/components/StrapiSettingForm.js
+++ frontend/src/components/StrapiSettingForm.js
@@ -8,12 +8,8 @@ export default class StrapiSettingForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            fields: {
-                baseUrl: ""
-            },
-            errors: {
-                baseUrl: ''
-            },
+            baseUrl: "",
+            error: '',
             loadingData: false
         };
     }
@@ -32,30 +28,15 @@ export default class StrapiSettingForm extends Component {
 
     handleUserInput = e => {
         this.setState({
-            errors: {
-                ...this.state.errors,
-                [e.target.name]: this.validate(e.target.value)
-            },
-            fields: {
-                ...this.state.fields,
-                [e.target.name]: e.target.value
-            }
+            error: this.validate(e.target.value),
+            baseUrl: e.target.value
         });
     };
 
     handleSubmit = e => {
-        const { fields } = this.state;
         e.preventDefault();
-        const error = this.validate(fields.baseUrl);
-        if (error) {
-            this.setState({ errors: error });
-            return;
-        }
-        if (fields.baseUrl) {
-            const data = {
-                baseUrl: fields.baseUrl
-            };
-            this.callSaveStrapiConfiguration(data);
+        if (this.state.baseUrl) {
+            this.callSaveStrapiConfiguration({ baseUrl: this.state.baseUrl });
         }
     };
 
@@ -66,9 +47,9 @@ export default class StrapiSettingForm extends Component {
     async callSaveStrapiConfiguration(payload) {
         this.setState({ loadingData: true });
         const { data, isError } = await saveStrapiConfiguration(payload);
-        if (data && !isError) {
+        if (data && data.data && !isError) {
             this.setState({
-                fields: { baseUrl: data.data.baseUrl }
+                baseUrl: data.data.baseUrl
             });
         } else if (data && isError) {
             console.error(data);
@@ -81,67 +62,67 @@ export default class StrapiSettingForm extends Component {
         const { data, isError } = await getStrapiConfiguration();
         if (data && data.data && !isError) {
             this.setState({
-                fields: { baseUrl: data.data.baseUrl }
+                baseUrl: data.data.baseUrl
             });
         }
         this.setState({ loadingData: false });
     }
 
     render() {
-        const { errors } = this.state;
+        const { error, baseUrl } = this.state;
         return (
             <div>
-            <form autoComplete="off" onSubmit={this.handleSubmit}>
-                <Grid>
-                    <Row className="mt-2">
-                        <Col lg={12}>
-                            <h1><b>{LABEL_STRAPI_CONFIG_SETTINGS}</b></h1>
-                        </Col>
-                    </Row>
-                    <Row className="mt-2"></Row>
-                    <Row className="mt-2">
-                        <Col lg={3}>
-                            <label htmlFor="type" className="control-label">
-                                <span className="FormLabel">
-                                    <span>{LABEL_APPLICATION_URL}</span>
-                                    <sup>
-                                        <i className="fa fa-asterisk required-icon FormLabel__required-icon"></i>
-                                    </sup>
-                                </span>
-                            </label>
-                            <FieldLevelHelp buttonClass="" close={undefined} content={TOOLTIP_URL} inline placement="right" rootClose />
-                        </Col>
-                        <Col lg={5}>
-                            <input
-                                name="baseUrl"
-                                type="text"
-                                id="id"
-                                value={this.state.fields.baseUrl}
-                                placeholder=""
-                                className="form-control RenderTextInput"
-                                onChange={event => this.handleUserInput(event)}
-                            />
-                            <span className="text-danger">{errors.baseUrl}</span>
-                            {this.state.loadingData &&
-                                <Spinner
-                                loading={this.state.loadingData}
-                                className=""
-                                size="md"
-                                ></Spinner>}
-                        </Col>
-                        <Col lg={4}></Col>
-                    </Row>
-                    <Row>
-                        <Col lg={8}></Col>
-                        <Col lg={4}>
-                            <button className="btn-primary btn"
-                                disabled={this.state.errors.baseUrl.length}>{BUTTON_SAVE}
-                            </button>
-                        </Col>
-                    </Row>
-                </Grid>
-            </form>
-        </div>
+                <form autoComplete="off" onSubmit={this.handleSubmit}>
+                    <Grid>
+                        <Row className="mt-2">
+                            <Col lg={12}>
+                                <h1><b>{LABEL_STRAPI_CONFIG_SETTINGS}</b></h1>
+                            </Col>
+                        </Row>
+                        <Row className="mt-2"></Row>
+                        <Row className="mt-2">
+                            <Col lg={3}>
+                                <label htmlFor="type" className="control-label">
+                                    <span className="FormLabel">
+                                        <span>{LABEL_APPLICATION_URL}</span>
+                                        <sup>
+                                            <i className="fa fa-asterisk required-icon FormLabel__required-icon"></i>
+                                        </sup>
+                                    </span>
+                                </label>
+                                <FieldLevelHelp buttonClass="" close={undefined} content={TOOLTIP_URL} inline placement="right" rootClose />
+                            </Col>
+                            <Col lg={5}>
+                                <input
+                                    name="baseUrl"
+                                    type="text"
+                                    id="id"
+                                    value={baseUrl}
+                                    placeholder=""
+                                    className="form-control RenderTextInput"
+                                    onChange={event => this.handleUserInput(event)}
+                                />
+                                <span className="text-danger">{error}</span>
+                                {this.state.loadingData &&
+                                    <Spinner
+                                        loading={this.state.loadingData}
+                                        className=""
+                                        size="md"
+                                    ></Spinner>}
+                            </Col>
+                            <Col lg={4}></Col>
+                        </Row>
+                        <Row>
+                            <Col lg={8}></Col>
+                            <Col lg={4}>
+                                <button className="btn-primary btn"
+                                    disabled={!baseUrl || error}>{BUTTON_SAVE}
+                                </button>
+                            </Col>
+                        </Row>
+                    </Grid>
+                </form>
+            </div>
         )
     }
 }
