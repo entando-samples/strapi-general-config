297873190a5ed430a37576913b650bc37c79c93e
Kamlesh Bobde <kamlesh.bobde@newvisionsoftware.in>
2022-08-01 16:22:18 +0530

    PBCS-134: updated strapi url in constants as per integration env

--- frontend/src/helpers/constants.js
+++ frontend/src/helpers/constants.js
@@ -1,5 +1,5 @@
 
-export const TOOLTIP_URL = 'URL of the application, Example - http://142.40.0.142:1337'
+export const TOOLTIP_URL = 'URL of the application, Example - http://20.113.131.137:1337'
 export const LABEL_APPLICATION_URL = 'Application URL'
 export const MSG_REQ_APPLICATION_URL = 'Application URL is Required'
 export const MSG_VALID_APPLICATION_URL = 'Enter a valid application URL'
